8203512c2d95c731b18903cbc5c216a1acc94abc
Javier Gomez <javiergomezve@gmail.com>
2019-01-05 14:16:26 -0400

    First request

---
+++ app/src/main/java/com/javiergomezve/foodtruck/activities/FoodTruckListActivity.java
@@ -0,0 +1,74 @@
+package com.javiergomezve.foodtruck.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.javiergomezve.foodtruck.R;
+import com.javiergomezve.foodtruck.model.FoodTruck;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FoodTruckListActivity extends AppCompatActivity {
+
+    private final String TAG = FoodTruckListActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_truck_list);
+
+        String url = "http://10.0.2.2:3005/api/v1/foodtruck";
+        final ArrayList<FoodTruck> foodTruckList = new ArrayList<>();
+
+        final JsonArrayRequest getTrucks = new JsonArrayRequest(
+                Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONArray foodTrucks = response;
+                            for (int x = 0; x < foodTrucks.length(); x++) {
+                                JSONObject foodTruck = foodTrucks.getJSONObject(x);
+                                String name = foodTruck.getString("name");
+                                String id = foodTruck.getString("_id");
+                                String foodType = foodTruck.getString("foodType");
+                                Double avgCost = foodTruck.getDouble("avgcost");
+
+                                JSONObject geometry = foodTruck.getJSONObject("geometry");
+                                JSONObject coordinates = geometry.getJSONObject("coordinates");
+
+                                Double latitude = coordinates.getDouble("lat");
+                                Double longitude = coordinates.getDouble("long");
+
+                                FoodTruck newFoodTruck = new FoodTruck(id, name, foodType, avgCost, latitude, longitude);
+                                foodTruckList.add(newFoodTruck);
+                            }
+
+                            Log.i(TAG, foodTruckList.get(0).getName());
+                        } catch (JSONException e){
+                            Log.v("API", "EXC " + e.getLocalizedMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("API", "Err " + error.getLocalizedMessage());
+                    }
+                }
+        );
+
+        Volley.newRequestQueue(this).add(getTrucks);
+    }
+}
